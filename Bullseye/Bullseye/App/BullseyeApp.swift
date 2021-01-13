fa0bc49b6cda8d8cfa879c73e38b6aabfbf4bd1f
J053-G <72273743+J053-G@users.noreply.github.com>
2021-01-13 12:30:13 -0600

    Add files via upload

---
+++ Bullseye/Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by P.M. Student on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
